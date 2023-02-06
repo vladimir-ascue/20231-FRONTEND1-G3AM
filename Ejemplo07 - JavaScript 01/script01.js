c30ab7a71d4666321f5509020ea8e6d50e97b88c
Vladimir Ilich Ascue Lovon <vladimir.ascue@vlaslov.com>
2023-02-06 10:02:36 -0500

    Java Script

---
+++ Ejemplo07 - JavaScript 01/script01.js
@@ -0,0 +1,46 @@
+/*
+for (let i = 0; i < 5; i++) {
+    document.writeln(
+        "<div className='card' style=width: 18rem; border: 1px solid #ccc;'>"+
+            "<div className='card-body'>"+
+                "<h5 className='card-title'>Card title</h5>"+
+                "<h6 className='card-subtitle mb-2 text-muted'>Card subtitle</h6>"+
+                "<p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card content.</p>"+
+                "<a href='#' className='card-link'>Card link</a>"+
+                "<a href='#' className='card-link'>Another link</a>"+
+            "</div>"+
+        "</div>");
+}
+
+*/
+
+function mostrar(){
+    document.getElementById("activador").innerText = "Ocultar";
+    document.getElementById("menu").style.setProperty("transform", "scaleX(1)");
+    document.getElementById("menu").style.setProperty("transition", ".3s");
+}
+
+function ocultar(){
+    document.getElementById("activador").innerText = "Mostrar";
+    document.getElementById("menu").style.setProperty("transform", "scaleX(0)");
+    document.getElementById("menu").style.setProperty("transition", ".3s");
+
+
+}
+
+function activar(){
+    var texto = document.getElementById("activador").innerText;
+    if (texto == "Mostrar"){
+        mostrar();
+    }else{
+        ocultar()
+    }
+}
+
+
+
+
+
+
+
+
