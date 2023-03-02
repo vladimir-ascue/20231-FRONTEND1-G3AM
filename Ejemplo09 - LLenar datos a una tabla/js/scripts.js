7c828c40e20c8ec1b153fc0b5419c09b9df8722b
vladimir-ascue <vladimir.ascue@gmail.com>
2023-03-02 12:54:34 -0500

    Actualizacion final de código

--- Ejemplo09 - LLenar datos a una tabla/js/scripts.js
+++ Ejemplo09 - LLenar datos a una tabla/js/scripts.js
@@ -1,10 +1,6 @@
 function llenarTabla(){
-    // datos = ["vladimir", "Ascue"];
-    // datos = fetch("Http://Servicioweb.com/datitosdeejemplo)
-
     datos = [
         ["158","vladimir", "Ascue Lovon", "vladimir.ascue@gmail.com"],
-//        ["vladimir", "Ascue"],
         ["321","Carlos", "Alcantara"],
         ["654","Vanessa", "Terques"],
         ["195","Don Quijote", "de la Mancha", "dquijote@mail.com", "Acuña"],
@@ -20,39 +16,24 @@ function llenarTabla(){
         }
     }
 
-    let numFilas = datos.length; // 3
-    let numColumnas = datos[0].length; //
-
-/*    for (let i = 0; i < numFilas; i++) {
-        document.write(
-            "<tr>" +
-            "<td>"+(i+1)+"</td>" +
-            "<td>"+datos[i][0]+"</td>" +
-            "<td>"+datos[i][1]+"</td>" +
-            "</tr>")
-    }*/
+    let numFilas = datos.length;
+//    let numColumnas = datos[0].length;
 
     for (let i = 0; i < numFilas; i++) {
-//        document.write("<tr class='dato'>") >> Cuando varios elementos usan el mismo Selector
         document.write("<tr id='"+datos[i][0]+"'>")
         document.write("<td>"+(i+1)+"</td>");
         for (let j = 1; j < 6; j++) {
-//            document.write("<td>"+(i+1)+"Ojito </td>");
             if(datos[i][j]==undefined)
                 document.write("<td></td>");
             else
                 document.write("<td>"+datos[i][j]+"</td>");
         }
-        document.write("<td>" +
-            "<button>Detalle</button>" +
-            "<button>Editar</button>" +
-            "<button>Eliminar</button>" +
-            "</td>");
-        document.write("</tr>")
     }
 }
 
 llenarTabla();
+
+// create.element
 
 const tabla = document.querySelector("#tablaDatos");
 tabla.addEventListener("click", (event) => {
@@ -61,6 +42,9 @@ tabla.addEventListener("click", (event) => {
         const nombre = fila.cells[1].textContent;
         const apellidos = fila.cells[2].textContent;
         const email = fila.cells[3].textContent;
+
+        fila.cells[3].textContent = "Pepe";
+
         document.querySelector("#nombre").value = nombre;
         document.querySelector("#apellidos").value = apellidos;
         document.querySelector("#email").value = email;
